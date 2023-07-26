b1302a93449d5e6731fe89e2f2f4d796e7cabd4a
KIPROTYCH <kiprotych@gmail.com>
2023-07-26 22:34:12 +0300

    created pages folder

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function About () {
+    return (
+      <>
+        <h1>This is my About Page</h1>
+        <ul>
+          <li>One </li>
+          <li>two </li>
+          <li>Three </li>
+        </ul>
+      </>
+    );
+};
+
+export default About;
---
+++ src/pages/Pricing.js
@@ -0,0 +1,5 @@
+export default function Pricing () {
+    return (
+        <h1>This is my Pricing Page</h1>
+    )
+}
