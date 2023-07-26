6a91fb86c87ab95f55b3b985ce52da505d897b4e
KIPROTYCH <kiprotych@gmail.com>
2023-07-26 19:39:53 +0300

    Created Navbar.js File

---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <a href="/" className="site-logo">
+        LOGO
+      </a>
+      <ul>
+        <li>
+          <a href="/pricing">Pricing</a>
+        </li>
+        <li>
+          <a href="/contacts">Contacts</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
