dd24a28175517650107df819a471ee176b0a9bb6
KIPROTYCH <kiprotych@gmail.com>
2023-07-26 22:34:41 +0300

    Modified Contents

--- src/App.js
+++ src/App.js
@@ -4,13 +4,29 @@ import Counter from "./Counter";
 import DataFetching from "./DataFetching";
 import Navbar from "./Navbar";
 import "./styles.css"
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Pricing from "./pages/Pricing";
 
 function App() {
+  let component 
+  switch (window.location.pathname) {
+    case "/":
+      component = <Home />;
+      break
+    case "/about":
+      component = <About />;
+      break
+    case "/pricing":
+      component = <Pricing />;
+      break
+  }
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
       <Counter />
-      <DataFetching/>
+      <DataFetching />
+      {component}
     </div>
   );
 }
